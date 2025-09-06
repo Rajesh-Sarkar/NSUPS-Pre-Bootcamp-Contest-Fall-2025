d6bd1d6551078d285bbb450685fbbcc5d80f6874
Rajesh Protap Sarkar <rpsrajesh1212@gmail.com>
2025-09-07 00:42:17 +0600

    Add files via upload

---
+++ Contest Question Solve/AE_Make_N_Zero.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        if(n==1 || n==2 || n==5)
+        {
+            cout <<"NO" << endl;
+        }
+        else
+    {
+        cout <<"YES" << endl;
+    }
+    //     int subtract = n-3;
+    //     int again_subtract;
+    //     if(subtract<0)
+    //     {
+    //         cout << "NO" << endl;
+    //     }
+    //     else if(n>0)
+    //     {
+    //         again_subtract = subtract-4;
+    //         cout <<"YES" << endl;
+    //     }
+    // }}
+    }
+    return 0;
+}
